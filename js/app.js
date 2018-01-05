71928eb2882499f0f65550485e0857f8260c6e42
João Henrique <jppulicidade@gmail.com>
2018-01-05 16:58:08 +0000

    successfuly imported from codepen and working

--- js/app.js
+++ js/app.js
@@ -1,79 +1,114 @@
 $(document)
     .ready(function () {
 
-        // STORES THE INPUTS
-        var inputs = [""];
-
-        // STRING TO STORE CURRENT INPUT
-        var totalString;
-
-        // OPERATORS ARRAY FOR VALIDATION WITHOUT THE "."
-        var operators1 = ["+", "-", "/", "*"];
-
-        // OPERATORS ARRAY FOR VALIDATION WITH THE "."
-        var operators2 = ["."];
-
-        // NUMBERS FOR VALIDATION
-        var nums = [
-            0,
-            1,
-            2,
-            3,
-            4,
-            5,
-            6,
-            7,
-            8,
-            9
-        ];
-
-        // USER INPUT
-        function getValue(input) {
-            if (operators2.includes(inputs[inputs.length - 1] === true && input === ".")) {
-                console.log("Duplicat '.'");
-            } else if (inputs.length === 1 && operators1.includes(input) === false) {
-                inputs.push(input);
-            } else if (operators1.includes(inputs[inputs.length - 1]) === false) {
-                inputs.push(input);
-            } else if (nums.includes(Number(input))) {
-                inputs.push(input);
-            }
-            update();
-        }
-
-        // UPDATE
-        function update() {
-            totalString = inputs.join("");
-            $("#calcSteps").html(totalString);
-        }
-
-        // DISPLAYS TOTAL
-        function getTotal() {
-            totalString = inputs.join("");
-            $("#calcSteps").html(eval(totalString));
-        }
-
-        // AC FUNCTION
-        $("a")
-            .on("click", function () {
-                if (this.id === "calcDeleteAll") {
-                    inputs = [""];
-                    update( // CE FUNCTION
-                    );
-                } else if (this.id === "calcBackOne") {
-                    while (inputs.length > 1) {
-                        inputs.pop();
-                        update();
-                        break // TOTAL SIGN FUNCTION;
-                    }
-                } else if (this.id === "calcTotal") {
-                    getTotal();
-                } else {
-                    if (inputs[inputs.length - 1].indexOf("+", "-", "/", "*", ".") === -1) {
-                        getValue(this.id);
-                    } else {
-                        getValue(this.id);
-                    }
+        function playerTurn(turn, id) {
+            var spotTaken = $("#" + id).text();
+            if (spotTaken === "#") {
+                count++;
+                $("#" + id).text(turn);
+                turns[id] = turn;
+                winCondition(turns, turn);
+                if (gameOn === false) {
+                    computerTurn();
+                    winCondition(turns, computersTurn);
                 }
-            })
+            }
+        }
+
+        //FUNCTION FO PLAYER TURN
+        function winCondition(turnArray, currentTurn) {
+            if (turnArray[0] === currentTurn && turnArray[1] === currentTurn && turnArray[2] === currentTurn) {
+                gameOn = true;
+                reset();
+                alert("Player " + currentTurn + " wins! (Top row across 0,1, and 2 spots)");
+            } else if (turnArray[2] === currentTurn && turnArray[4] === currentTurn && turnArray[6] === currentTurn) {
+                gameOn = true;
+                reset();
+                alert("Player " + currentTurn + " wins! (Top row across 2,4, and 6 spots)");
+            } else if (turnArray[0] === currentTurn && turnArray[3] === currentTurn && turnArray[6] === currentTurn) {
+                gameOn = true;
+                reset();
+                alert("Player " + currentTurn + " wins! (1st row down 0,3, and 6 spots)");
+            } else if (turnArray[0] === currentTurn && turnArray[4] === currentTurn && turnArray[8] === currentTurn) {
+                gameOn = true;
+                reset();
+                alert("Player " + currentTurn + " wins! (1st row diagonally across 0,4, and 8 spots)");
+            } else if (turnArray[1] === currentTurn && turnArray[4] === currentTurn && turnArray[7] === currentTurn) {
+                gameOn = true;
+                reset();
+                alert("Player " + currentTurn + " wins! (2nd row down 1,4, and 7 spots)");
+            } else if (turnArray[2] === currentTurn && turnArray[5] === currentTurn && turnArray[8] === currentTurn) {
+                gameOn = true;
+                reset();
+                alert("Player " + currentTurn + " wins! (3rd row down 2,5, and 8 spots)");
+            } else if (turnArray[2] === currentTurn && turnArray[5] === currentTurn && turnArray[8] === currentTurn) {
+                gameOn = true;
+                reset();
+                alert("Player " + currentTurn + " wins! (3rd row across 2,4, and 6 spots)");
+            } else if (turnArray[3] === currentTurn && turnArray[4] === currentTurn && turnArray[5] === currentTurn) {
+                gameOn = true;
+                reset();
+                alert("Player " + currentTurn + " wins! (Middle row across 3,4, and 5 spots)");
+            } else if (turnArray[6] === currentTurn && turnArray[7] === currentTurn && turnArray[8] === currentTurn) {
+                gameOn = true;
+                reset();
+                alert("Player " + currentTurn + " wins! (Bottom row across 6,7, and 8 spots)");
+            } else {
+                gameOn = false;
+            }
+        }
+
+        //FUNCTION FOR COMPUTERS TURN
+        function computerTurn() {
+            var taken = false;
+            while (taken === false && count !== 5) {
+                var computersMove = (Math.random() * 10).toFixed();
+                var move = $("#" + computersMove).text();
+                if (move === '#') {
+                    $('#' + computersMove).text(computersTurn);
+                    taken = true;
+                    turns[computersMove] = computersTurn;
+                }
+            }
+        }
+
+        $(".tic")
+            .click(function () {
+                var slot = $(this).attr('id');
+                playerTurn(turn, slot);
+            });
+
+        $("#turnX").click(function () {
+            reset();
+            turn = 'X';
+            computersTurn = 'O';
+            $("#turnO").removeClass("btn-primary");
+            $("#turnX").addClass("btn-primary");
+
+        });
+        $("#turnO").click(function () {
+            reset();
+            turn = "O";
+            computersTurn = 'X'
+            $("#turnX").removeClass("btn-primary");
+            $("#turnO").addClass("btn-primary");
+            $(".tic").text("#");
+        });
+
+        function reset() {
+            turns = [
+                "#",
+                "#",
+                "#",
+                "#",
+                "#",
+                "#",
+                "#",
+                "#",
+                "#"
+            ];
+            count = 0;
+            $(".tic").text("#");
+            gameOn = false;
+        }
     });
